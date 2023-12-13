b53eba1805d587808157c7de6828e77f057a1328
Sonia Zamora Martín <hechodeoroverde@gmail.com>
2023-12-14 00:49:21 +0100

    tarea finalizada

--- Tarea_6_Multiplicacion/multiplicar.php
+++ Tarea_6_Multiplicacion/multiplicar.php
@@ -5,38 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Multiplicación</title>
     <style>
-        td {
+        table {
+            border-collapse: collapse;
+            display: inline-flex;
+        }
+        table, th, td {
             border: 1px solid black;
         }
-        span {
+        th {
             background-color: lightblue;
-            font-weight: bold;
-            width: 190px;
-            
+            font-weight: bold;         
         }
-        
     </style>
 </head>
 <body>
+    <h2>Tablas de multiplicar</h2>
     <?php
     $tablas = ["primera" => 5, "segunda" => 13, "tercera" => 11];
     ?>
-    <table>
-        <tr>
-            <?php 
-            foreach ($tablas as $key => $value) {
-            ?>
-            <td>
-                <?php 
-                    echo "<span>Tabla $key</span>" . "<br>";
-
-                    for ($i=1; $i <= 10; $i++) { 
-                        echo "$value x $i = " . $value * $i . "<br>";
-                    }
-                }
-                ?>
-            </td>
-        </tr>
-    </table>
+        <!-- Iterar tantas tablas de multiplicar como tengamos en el array dentro del siguiente foreach -->
+        <?php foreach ($tablas as $key => $value) { ?>
+            <table>
+                <tr>
+                    <th><?php echo "Tabla $key"; ?></th>
+                </tr>
+                <?php for ($i=1; $i <= 10; $i++) { ?>
+                    <tr>
+                        <td>
+                        <?php echo "$value x $i = " . $value * $i . "<br>"; ?>
+                        </td>
+                    </tr>
+                <?php } // end for?>
+            </table>
+        <?php } // end foreach ?>
 </body>
 </html>
