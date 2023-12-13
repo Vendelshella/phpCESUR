2bfa18ed42c1d82b6d1d6265417f5d06a177d9d2
Sonia Zamora Martín <hechodeoroverde@gmail.com>
2023-12-13 20:40:14 +0100

    Tarea 6 primer commit

---
+++ Tarea_6_Multiplicacion/multiplicar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplicación</title>
+    <style>
+        td {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $tablas = ["primera" => 5, "segunda" => 13, "tercera" => 11];
+
+    
+    ?>
+    <table>
+        <tr>
+            <?php 
+            foreach ($tablas as $key => $value) {
+            ?>
+            <td>
+                <?php 
+                    echo "Tabla $key" . "<br>";
+
+                    for ($i=1; $i <= 10; $i++) { 
+                        
+                        echo "$value x $i = " . $value * $i . "<br>";
+                    }
+                }
+                ?>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
