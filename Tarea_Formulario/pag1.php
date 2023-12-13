194c9a23a311037a64d13af945a394550bc9957f
Sonia Zamora Martín <hechodeoroverde@gmail.com>
2023-12-13 19:59:41 +0100

    cambio de numero de tarea

---
+++ Tarea_Formulario/pag1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookies</title>
+</head>
+<body>
+    <!-- Formulario para introducir el nombre del usuario -->
+    <form action="pag2.php" method="post">
+        <label for="user_name">Nombre: <input type="text" name="user_name"></label>
+        <input type="submit" value="Enviar" name="send">
+    </form>
+</body>
+</html>
